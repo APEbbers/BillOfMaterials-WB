65f5baf36d1d01843357105d19e812775ab2eea4
APEbbers <paul.ebbers@gmail.com>
2024-02-24 20:28:40 +0100

    update version

--- Icons_rc.py
+++ Icons_rc.py
@@ -3,7 +3,7 @@
 # Created by: The Resource Compiler for Qt version 6.6.1
 # WARNING! All changes made in this file will be lost!
 
-from PySide6 import QtCore
+from PySide import QtCore
 
 qt_resource_data = b"\
 \x00\x00\x1a\x92\
@@ -5939,10 +5939,13 @@ qt_resource_struct = b"\
 \x00\x00\x01\x8d\xc2\x95g\xd7\
 "
 
+
 def qInitResources():
     QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
+
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
 
 qInitResources()
