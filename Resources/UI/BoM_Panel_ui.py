588b1e1a804e8f37de24f378f9c8d5c5d438347d
Paul Ebbers <paul.ebbers@gmail.com>
2024-07-13 18:00:19 +0200

    Update BoM_Panel_ui.py

--- Resources/UI/BoM_Panel_ui.py
+++ Resources/UI/BoM_Panel_ui.py
@@ -8,14 +8,14 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+from PySide.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+from PySide.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDialog,
+from PySide.QtWidgets import (QApplication, QCheckBox, QComboBox, QDialog,
     QFrame, QGridLayout, QLabel, QLayout,
     QPushButton, QSizePolicy, QSpacerItem, QSpinBox,
     QToolButton, QVBoxLayout, QWidget)
