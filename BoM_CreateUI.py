4c34b46490424a389e8ee897b29958b7d328ddc0
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-11-22 08:25:29 -0500

    Add SPDX License Identifiers

--- BoM_CreateUI.py
+++ BoM_CreateUI.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
 # ***************************************************************************
 # *   Copyright (c) 2023 Paul Ebbers paul.ebbers@gmail.com                  *
 # *                                                                         *
