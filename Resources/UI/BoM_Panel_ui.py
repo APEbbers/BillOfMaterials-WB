84d14f15ca3fddaefb472d2e58c2f7711bb9f537
APEbbers <paul.ebbers@gmail.com>
2026-07-10 14:43:57 +0200

    Changed Arch to BIM

--- Resources/UI/BoM_Panel_ui.py
+++ Resources/UI/BoM_Panel_ui.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'BoM_PanelnDTKDt.ui'
+## Form generated from reading UI file 'BoM_PanelBWIvsX.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.11.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide.QtWidgets import (QApplication, QCheckBox, QComboBox, QDialog,
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDialog,
     QFrame, QGridLayout, QGroupBox, QLabel,
     QLayout, QLineEdit, QPushButton, QSizePolicy,
     QSpacerItem, QSpinBox, QToolButton, QVBoxLayout,
     QWidget)
-import Icons_rc
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
